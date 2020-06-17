d9988b9ea589484e0a2aa3dd249e41dcb92e4d4f
velrino <denismagalhaesvelrino@gmail.com>
2020-06-17 02:00:12 -0300

    :lipstick: Added Creative Tim template

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-admin',
-    templateUrl: './admin.component.html'
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent { }
