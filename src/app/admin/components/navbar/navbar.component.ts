46751570b72b4a53b071fe7e9b553ad2181b3259
velrino <denismagalhaesvelrino@gmail.com>
2020-06-19 01:31:46 -0300

    :art: Implemented logout in NavBar

--- src/app/admin/components/navbar/navbar.component.ts
+++ src/app/admin/components/navbar/navbar.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
     selector: 'app-admin-navbar',
     templateUrl: './navbar.component.html',
 })
-export class AdminNavbarComponent { }
+export class AdminNavbarComponent {
+    constructor(private authService: AuthService, private router: Router) { }
+
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['admin/auth/login']);
+    }
+}
