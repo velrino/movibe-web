ad7b01a1a4862ff194fb677c73d1fd593769ab36
velrino <denismagalhaesvelrino@gmail.com>
2020-06-19 01:24:59 -0300

    :art: Implemented logout in sidebar

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 @Component({
     selector: 'app-admin-sidebar',
     templateUrl: './sidebar.component.html',
 })
-export class AdminSidebarComponent { }
+export class AdminSidebarComponent {
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    logout() {
+        this.authService.logout();
+        this.router.navigate(['admin/auth/login']);
+    }
+}
