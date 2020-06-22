92205ca82a01ca066af9330b26812d2579a8fb92
velrino <denismagalhaesvelrino@gmail.com>
2020-06-21 23:48:44 -0300

    :zap: Implemented dynamic menu with active class

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -8,8 +8,28 @@ import { AuthService } from 'src/app/shared/services/auth/auth.service';
     templateUrl: './sidebar.component.html',
 })
 export class AdminSidebarComponent {
+    actualPath: string = null;
+    basePath: string = '/admin';
 
     constructor(private authService: AuthService, private router: Router) { }
+
+    menu = [
+        { name: 'Dashboard', route: '', icon: "nc-icon nc-chart-pie-35" },
+        { name: 'Baladas', route: '/ballads', icon: "fas fa-glass-cheers" }
+    ]
+
+    setActualPath() {
+        this.actualPath = this.router.url;
+    }
+
+    isActualPath(route: string) {
+        this.setActualPath();
+        return (this.actualPath === this.basePath.concat(route));
+    }
+
+    navigate(route: string) {
+        this.router.navigate([this.basePath.concat(route)]);
+    }
 
     logout() {
         this.authService.logout();
